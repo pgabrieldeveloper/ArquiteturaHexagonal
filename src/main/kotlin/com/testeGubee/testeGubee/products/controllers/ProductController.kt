59efa9ebed8395135c5991063c99d42b1fbd0c7f
gabriel <g4brieldev@gmail.com>
2021-08-25 22:11:08 -0300

    criacao de service de pesquisar por target e stack

--- src/main/kotlin/com/testeGubee/testeGubee/products/controllers/ProductController.kt
+++ src/main/kotlin/com/testeGubee/testeGubee/products/controllers/ProductController.kt
@@ -4,6 +4,7 @@ import com.testeGubee.testeGubee.products.Dtos.ParametersFinds
 import com.testeGubee.testeGubee.products.entities.Product
 import com.testeGubee.testeGubee.products.services.FindByStack
 import com.testeGubee.testeGubee.products.services.FindByTargetMarket
+import com.testeGubee.testeGubee.products.services.FindByTargetMarketAndStack
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,15 +14,21 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/product")
-class ProductController(private val serviceFindByStack:FindByStack, private val serviceFindByTargetMarket: FindByTargetMarket) {
+class ProductController(private val serviceFindByStack:FindByStack, private val serviceFindByTargetMarket: FindByTargetMarket, private val serviceserviceFindByTargetMarketAndStack: FindByTargetMarketAndStack) {
 
     @PostMapping("findByStack")
     fun findByStack(@RequestBody parametersFinds: ParametersFinds):ResponseEntity<List<Product>>{
         return ResponseEntity.ok(serviceFindByStack.find(parametersFinds))
     }
+
     @PostMapping("findByTargetMarket")
     fun findBytargetMarket(@RequestBody parametersFinds: ParametersFinds):ResponseEntity<List<Product>>{
         return ResponseEntity.ok(serviceFindByTargetMarket.find(parametersFinds))
+    }
+
+    @PostMapping("findByTargetMarketAndStack")
+    fun findByTargetMarketAndStack(@RequestBody parametersFinds: ParametersFinds): ResponseEntity<List<Product>>{
+        return ResponseEntity.ok(serviceserviceFindByTargetMarketAndStack.find(parametersFinds))
     }
 
 }
--- src/main/kotlin/com/testeGubee/testeGubee/products/services/FindByTargetMarketAndStack.kt
+++ src/main/kotlin/com/testeGubee/testeGubee/products/services/FindByTargetMarketAndStack.kt
@@ -7,9 +7,9 @@ import com.testeGubee.testeGubee.products.repository.ProductRepository
 import org.springframework.stereotype.Service
 
 @Service
-class FindByTargetMarket(val repository: ProductRepository):Finds {
+class FindByTargetMarketAndStack(private val repository: ProductRepository):Finds {
     override fun find(parametersFinds: ParametersFinds): List<Product> {
         val listProducts:List<Product> = this.repository.findAll();
-        return listProducts.filter { Contains.contains(parametersFinds.targetMarket, it.targetMarket)};
+        return listProducts.filter {Contains.contains(parametersFinds.targetMarket, it.targetMarket) && Contains.contains(parametersFinds.stack, it.stack) };
     }
 }
